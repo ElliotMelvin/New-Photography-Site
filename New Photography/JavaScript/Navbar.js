6c1a549e1ca67b850366cbeb8e2989d5be4678e5
Elliot <39118700+ElliotMelvin@users.noreply.github.com>
2018-05-18 16:31:24 +0100

    Add files via upload

---
+++ New Photography/JavaScript/Navbar.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+$(window).scroll(function() {
+// 100 = The point you would like to fade the nav in.
+  
+	if ($(window).scrollTop() > 100 ){
+    
+ 		$('.topnav').addClass('show');
+    
+  } else {
+    
+    $('.topnav').removeClass('show');
+    
+ 	};   	
+});
+
+$('.scroll').on('click', function(e){		
+		e.preventDefault()
+    
+  $('html, body').animate({
+      scrollTop : $(this.hash).offset().top
+    }, 1500);
+});
