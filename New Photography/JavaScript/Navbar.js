e5631b2d2efa9240ac7bccafc92d3f216ff8ad3a
ElliotMelvin <elliot221@live.co.uk>
2018-05-19 19:24:33 +0100

    m

--- New Photography/JavaScript/Navbar.js
+++ New Photography/JavaScript/Navbar.js
@@ -1,22 +1,27 @@
-// JavaScript Document
-$(window).scroll(function() {
-// 100 = The point you would like to fade the nav in.
-  
-	if ($(window).scrollTop() > 100 ){
-    
- 		$('.topnav').addClass('show');
-    
-  } else {
-    
-    $('.topnav').removeClass('show');
-    
- 	};   	
+//Wait for page to load
+window.onload = function(){
+	
+  var element = document.querySelector(".active");
+  element.addEventListener("click", function(e){
+    scrollIt("#top",800);
+  });
+
+var element = document.querySelector(".image");
+element.addEventListener("click", function(e){
+  scrollIt("#images",800);
 });
 
-$('.scroll').on('click', function(e){		
-		e.preventDefault()
-    
-  $('html, body').animate({
-      scrollTop : $(this.hash).offset().top
-    }, 1500);
+var element = document.querySelector(".about");
+element.addEventListener("click", function(e){
+  scrollIt("#about",800);
 });
+
+var element = document.querySelector(".contact");
+element.addEventListener("click", function(e){
+  scrollIt("#contact",800);
+});
+var element = document.querySelector(".container-arrow");
+element.addEventListener("click", function(e){
+  scrollIt("#images",800);
+});
+};
