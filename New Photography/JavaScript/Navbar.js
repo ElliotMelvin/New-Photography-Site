fe3e46c5ecab184bdd057c76f8429cbf62ccbe5a
ElliotMelvin <elliot221@live.co.uk>
2018-05-20 12:30:53 +0100

    Text

--- New Photography/JavaScript/Navbar.js
+++ New Photography/JavaScript/Navbar.js
@@ -24,4 +24,25 @@ var element = document.querySelector(".container-arrow");
 element.addEventListener("click", function(e){
   scrollIt("#images",800);
 });
+	$(window).scroll(function() {
+// 100 = The point you would like to fade the nav in.
+  
+	if ($(window).scrollTop() > 100 ){
+    
+ 		$('.bg').addClass('show');
+    
+  } else {
+    
+    $('.bg').removeClass('show');
+    
+ 	};   	
+});
+
+$('.scroll').on('click', function(e){		
+		e.preventDefault()
+    
+  $('html, body').animate({
+      scrollTop : $(this.hash).offset().top
+    }, 1500);
+});
 };
