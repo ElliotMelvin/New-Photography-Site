f96513896434b435787da2a2cb811e8d833f01ce
Elliot <39118700+ElliotMelvin@users.noreply.github.com>
2018-05-29 15:29:29 +0100

    Add files via upload

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -15,6 +15,10 @@ var element = document.querySelector(".about");
 element.addEventListener("click", function(e){
   scrollIt("#about",800);
 });
+	var element = document.querySelector(".videos");
+element.addEventListener("click", function(e){
+  scrollIt("#videos",800);
+});
 
 var element = document.querySelector(".contact");
 element.addEventListener("click", function(e){
@@ -28,7 +32,7 @@ element.addEventListener("click", function(e){
 	
 
 // -----------------------Image Gallery
-window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: false });
 
 // Custom Settings
 sr.reveal('.one', { duration: 600 });
@@ -47,8 +51,42 @@ sr.reveal('.four', {
 sr.reveal('.five', { 
   duration: 200 
 });
+	sr.reveal('.textOne', { duration: 600 });
 	
-// -----------------------Nav Bar Scroll
+	sr.reveal('.textThree', { 
+  origin: 'right', 
+  duration: 2000 
+});
 	
-
+	sr.reveal('.textTwo', { 
+  viewFactor: 0.5
+});
+// -----------------------Video Hover
+	// ----Video One
+	var $video = $('.videoO');
+	
+	$video.on('mouseenter focus', function () {
+		$video.get(0).play();
+	});
+	$video.on('mouseout blur', function (){
+		$video.get(0).pause();
+	});
+	// ----Video Two
+	var $video2 = $('.videoT');
+	
+	$video2.on('mouseenter focus', function () {
+		$video2.get(0).play();
+	});
+	$video2.on('mouseout blur', function (){
+		$video2.get(0).pause();
+	});
+	// ----Video Three
+	var $video3 = $('.videoTh');
+	
+	$video3.on('mouseenter focus', function () {
+		$video3.get(0).play();
+	});
+	$video3.on('mouseout blur', function (){
+		$video3.get(0).pause();
+	});
 };
