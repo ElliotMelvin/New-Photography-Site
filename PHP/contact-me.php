76cced053fd7a857f11c9c750e99e77ee35f4298
Elliot <39118700+ElliotMelvin@users.noreply.github.com>
2018-06-19 07:50:31 +0100

    Add files via upload

--- PHP/contact-me.php
+++ PHP/contact-me.php
@@ -1,15 +1,56 @@
 <?php
-if (isset($_POST['submit'])){
-	$name = $_POST['name'];
-	$subject = $_POST['subject'];
-	$mailFrom = $_POST['mail'];
-	$message = $_POST['message'];
-	  
-	$mailTo = "elliot221@live.co.uk";
-	$headers = "From: ElliotP".$mailfrom;
-	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
-	
-	mail($mailTo, $txt,$headers);
-	header("Location: index.php?mailsend");
+if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' ) {
+
+    require '../includes/class.phpmailer.php';
+
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->SMTPSecure = 'tls';
+    $mail->Host = "smtp-mail.outlook.com"; // SMTP server example
+    $mail->SMTPDebug = 0; // enables SMTP debug information (for testing)
+    $mail->SMTPAuth = true; // enable SMTP authentication
+    $mail->Port = 587; // set the SMTP port for the GMAIL server
+    $mail->Username = "elliot221@live.co.uk"; // SMTP account username example
+    $mail->Password = "Good.bag1968!"; // SMTP account password example
+    
+
+    $to = "elliot221@live.co.uk";
+    $name = $_POST[ 'name' ];
+    $subject = $_POST[ 'subject' ];
+    $email = $_POST[ 'email' ];
+    $message = $_POST[ 'message' ];
+//    $body = 'From: ' . $name . ' <br> E-Mail: ' . $email . '<br> Subject: ' . $subject . '<br><br> Message: <br>' . $message;
+    $body = '<b>Name: </b>' . $name . nl2br() . '<b>Email: </b>' . $email . $subject . $message;
+    
+    // Recipients
+    $mail->setFrom('elliot221@live.co.uk', 'Elliot Photography');
+    $mail->addAddress($to, $name);     // Add a recipient
+//    $mail->addReplyTo('info@example.com', 'Information');
+//    $mail->addCC('cc@example.com');
+//    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+//    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $body;
+//    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+
+    //    echo 'The message is: ' . $message;
+
+
+    //	$headers = "From: elliot@;
+    //	$msg = "You have received an e-mail from {$name} \n\n {$message}";
+    //	
+    //	mail($to, $subject, $msg);
+    //    
+    //    header("Location: http://elliotphotography.x10.mx/thank-you.html");
+} else {
+    echo 'error';
 }
-?>
